88210d7ac7244ebef3261495c4da6fb4fbca2aea
Anton Semenov <ansemennov@gmail.com>
2023-05-28 19:14:39 +0500

    Подключение пакета frontend

--- service/diplom/settings.py
+++ service/diplom/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'diploma-frontend.frontend.apps',
+    'myauth',
 ]
 
 MIDDLEWARE = [
