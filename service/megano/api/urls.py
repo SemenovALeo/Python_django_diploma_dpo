e5ee2b4d0928b875dc18c03b09140f6505270988
Anton Semenov <ansemennov@gmail.com>
2023-06-14 00:27:16 +0500

    создал приложение для каталога и продукта

--- service/megano/api/urls.py
+++ service/megano/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('banners', views.banners),
-    path('categories', views.categories),
-    path('catalog', views.catalog),
+    # path('banners', views.banners),
+    # path('categories', views.categories),
+    # path('catalog', views.catalog),
     path('products/popular', views.productsPopular),
     path('products/limited', views.productsLimited),
     path('sales', views.sales),
--- service/megano/megano/settings.py
+++ service/megano/megano/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'diploma-frontend.frontend.apps',
     'api',
     'user_profile',
+    'catalog',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -106,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
